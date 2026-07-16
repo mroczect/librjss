4dd7cc61f1c3e8b762c9a08f4cd3303dd99617c3
mroczect <mroczect@proton.me>
2026-07-16 23:32:35 +0700

    feat: export session_store module publicly

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,8 +1,10 @@
 pub mod config;
 pub mod env;
 pub mod error;
+pub mod session_store;
 pub mod types;
 
 pub use self::config::*;
 pub use self::error::*;
+pub use self::session_store::*;
 pub use self::types::*;
