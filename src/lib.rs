9056174eddc2b926ee0e610f5e85395108730024
mroczect <mroczect@proton.me>
2026-07-17 21:00:40 +0700

    feat(lib): export new types

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,7 @@ pub mod handler;
 pub use client::RjssClient;
 pub use handler::config::{AuthMode, ClientConfig};
 pub use handler::error::JssError;
-pub use handler::types::FrappeBoot;
-pub use handler::types::UserInfo;
+pub use handler::types::{
+    AppData, BootUser, BootUserInfo, FrappeBoot, FrequentLink, NavbarItem, NavbarSettings,
+    PageInfo, ReportMeta, SessionInfo, SidebarPage, SidebarPages, SysDefaults, UserInfo,
+};
