9150c9356c872229fd2c074edb8ec6e302ad4db6
mroczect <mroczect@proton.me>
2026-07-17 18:24:30 +0700

    feat(client): add token session setup

---
+++ src/client/auth/token.rs
@@ -0,0 +1,14 @@
+use crate::handler::types::session::SessionInfo;
+use secrecy::SecretString;
+use tracing::info;
+
+pub(crate) fn setup_token_session(expected_sitename: Option<String>) -> SessionInfo {
+    info!("Using token API key");
+    SessionInfo {
+        sid: SecretString::new(Box::from("token-mode")),
+        csrf_token: SecretString::new(Box::from("not-used")),
+        full_name: None,
+        sitename: expected_sitename.unwrap_or_default(),
+        roles: vec![],
+    }
+}
