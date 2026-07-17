90979d27e1593835ee8e81202ff4302bff04b2a4
mroczect <mroczect@proton.me>
2026-07-17 18:26:25 +0700

    docs(handler): remove doc comments from user info types

--- src/handler/types/user_info.rs
+++ src/handler/types/user_info.rs
@@ -1,12 +1,10 @@
 use serde::Deserialize;
 
-/// Response from the `get_logged_user` endpoint.
 #[derive(Debug, Deserialize)]
 pub struct UserInfo {
     pub message: UserInfoMessage,
 }
 
-/// Message payload inside a user info response.
 #[derive(Debug, Deserialize)]
 pub struct UserInfoMessage {
     pub name: String,
