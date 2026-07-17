a5d3f6cbfc37708bc85c969191d339363f134738
mroczect <mroczect@proton.me>
2026-07-17 18:24:30 +0700

    feat(client): add session ensure functionality

---
+++ src/client/auth/session.rs
@@ -0,0 +1,26 @@
+use crate::api::auth::AuthEndpoints;
+use crate::client::RjssClient;
+use crate::handler::error::JuraganError;
+use tracing::warn;
+
+pub(crate) async fn ensure_session(client: &mut RjssClient) -> Result<(), JuraganError> {
+    loop {
+        if client.session.is_none() {
+            if let Some((email, password)) = client.credentials.clone() {
+                super::login::login_with_credentials(client, email, password).await?;
+                return Ok(());
+            } else {
+                return Err(JuraganError::NotAuthenticated);
+            }
+        } else {
+            let url = RjssClient::get_logged_user_url(&client.config.base_url);
+            let resp = client.http.get(url).send().await?;
+            if resp.status() == reqwest::StatusCode::UNAUTHORIZED {
+                warn!(trace_id = client.trace_id, "Session expired, will re-login");
+                client.session = None;
+            } else {
+                return Ok(());
+            }
+        }
+    }
+}
