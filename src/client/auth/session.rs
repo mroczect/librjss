049e0161f9a449742c29bcee8cf3b266297abb44
mroczect <mroczect@proton.me>
2026-07-17 19:39:56 +0700

    refactor(client): rename JuraganError to JssError

--- src/client/auth/session.rs
+++ src/client/auth/session.rs
@@ -1,16 +1,16 @@
 use crate::api::auth::AuthEndpoints;
 use crate::client::RjssClient;
-use crate::handler::error::JuraganError;
+use crate::handler::error::JssError;
 use tracing::warn;
 
-pub(crate) async fn ensure_session(client: &mut RjssClient) -> Result<(), JuraganError> {
+pub(crate) async fn ensure_session(client: &mut RjssClient) -> Result<(), JssError> {
     loop {
         if client.session.is_none() {
             if let Some((email, password)) = client.credentials.clone() {
                 super::login::login_with_credentials(client, email, password).await?;
                 return Ok(());
             } else {
-                return Err(JuraganError::NotAuthenticated);
+                return Err(JssError::NotAuthenticated);
             }
         } else {
             let url = RjssClient::get_logged_user_url(&client.config.base_url);
